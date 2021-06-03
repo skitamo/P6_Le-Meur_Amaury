b884c8b1d5e7c260113d9dd89718faf29bf4d5f1
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-06-03 11:38:44 +0200

    Démmarage du serveur node

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,6 @@
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const express = require('express');
+const http = require('http')
 
-const options = {
-	key: fs.readFileSync('selfsigned.key'),
-	cert: fs.readFileSync('selfsigned.crt')
-};
-
-var app = express();
-
-const server = https.createServer((req, res) => {
+const server = http.createServer((req, res) => {
 	res.end('Voilà la réponse du serveur !');
 });
 
