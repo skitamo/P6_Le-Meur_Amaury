d9e12310ff8765ed5a9b031124fafe8c9f7438c0
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-06-04 16:38:39 +0200

    MAJ du serveur

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,59 @@
+// Importation des modules
 const http = require('http');
-const app = require('./app'); /* On appelle l'application */
+const app = require('./app');
 
-app.set('port', process.env.PORT || 3000); /* On définit sur quel port l'application doit tourner */
+
+// Configuration du port de connection en fonction de l'environnement
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+
+// Ajout du port de connection si celui-ci n'est pas declarer par l environnement
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+
+
+// Recherche et gestion de différentes erreurs
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Création du serveur avec express
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+// Gestions des évenements serveur
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+// Ecoute du serveur au port 3000
+server.listen(port);
