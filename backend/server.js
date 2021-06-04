b9b77ce7769dd47078da9e4b8cebf478725af540
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-06-04 10:21:59 +0200

    Exécution de l'application Express sur le serveur Node

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const http = require('http')
+const http = require('http');
+const app = require('./app'); /* On appelle l'application */
 
-const server = http.createServer((req, res) => {
-	res.end('Voilà la réponse du serveur !');
-});
+app.set('port', process.env.PORT || 3000); /* On définit sur quel port l'application doit tourner */
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
