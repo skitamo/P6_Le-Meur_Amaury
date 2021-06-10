6d3742f4294d61776b8665569ac2545788a0ed1f
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-06-10 10:19:42 +0200

    Ajout du controller PUT pour modifier une sauce et la renvoyer dans la base de donnée

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -16,5 +16,17 @@ exports.createSauce = (req, res, next) => {
 	});
 	sauce.save()
 	.then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-	.catch(error => res.status(400).json({ error }))
+	.catch(error => res.status(400).json({ error }));
+};
+
+/* Route PUT pour modifier une sauce et la renvoyer dans la base de donnée */
+exports.modifySauce = (req, res, next) => {
+	const sauceObject = req.file ?
+	{
+		...JSON.parse(req.body.sauce),
+		imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+	} : { ...req.body };
+	Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+		.then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+		.catch(error => res.status(400).json({ error }));
 };
