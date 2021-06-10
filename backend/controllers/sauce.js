ca61aa93b1555b212828ca2011974f10b9cff46a
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-06-10 10:28:21 +0200

    Ajout du controller DELETE pour supprimer une sauce de la base de données

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -30,3 +30,17 @@ exports.modifySauce = (req, res, next) => {
 		.then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
 		.catch(error => res.status(400).json({ error }));
 };
+
+/* Route DELETE pour supprimer une sauce de la base de donnée */
+exports.deleteSauce = (req, res, next) => {
+	Sauce.findOne({ _id: req.params.id })
+	.then(sauce => {
+		const filename = sauce.imageUrl.split('/images/')[1];
+		fs.unlink(`images/${filename}`, () => {
+			Sauce.deleteOne({ _id: req.params.id })
+				.then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+				.catch(error => res.status(400).json({ error }));
+		});
+	})
+	.catch(error => res.status(500).json({ error }));
+};
