05b842a80d22b37eefdbe2ca3b710886c09adb05
Amaury Le Meur <amaury.lemeur@gmail.com>
2021-06-17 17:42:14 +0200

    Ajout d'un comparatif des sauces existantes lors de l'ajout d'une nouvelle sauce pour éviter d'ajouter un doublon

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -6,17 +6,24 @@ const fs = require('fs');
 exports.createSauce = (req, res, next) => {
 	const sauceObject = JSON.parse(req.body.sauce);
 	delete sauceObject._id;
-	const sauce = new Sauce({
-		...sauceObject,
-		imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-		likes: 0,
-		dislikes: 0,
-		usersLiked: [],
-		usersDisliked: []
-	});
-	sauce.save()
-	.then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-	.catch(error => res.status(400).json({ error }));
+	Sauce.findOne({ name:sauceObject.name })
+		.then(sauce => {
+			if (!sauce) {
+					const sauce = new Sauce({
+					...sauceObject,
+					imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+					likes: 0,
+					dislikes: 0,
+					usersLiked: [],
+					usersDisliked: []
+				});
+				sauce.save()
+				.then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
+				.catch(error => res.status(400).json({ error }));
+			}
+			else {console.log(alert('Sauce déjà existante !'))} 
+		})
+		.catch(error => res.status(404).json({ error }));
 };
 
 /* Route PUT pour modifier une sauce et la renvoyer dans la base de données */
